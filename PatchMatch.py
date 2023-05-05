78bb4884594a429d496eb091562eb743d6d93c22
Casey Quinn <74877883+cquinn2020@users.noreply.github.com>
2023-05-05 16:10:57 -0500

    Cleaned up the code.

--- PatchMatch.py
+++ PatchMatch.py
@@ -9,9 +9,9 @@ import scipy
 import copy
 
 matplotlib.use('TkAgg')
-# howdy
 
-def PoissonBlend(source, mask, target, isMix = False):
+
+def PoissonBlend(source, mask, target, isMix=False):
     target = np.array(target)
     rt = np.copy(target[:, :, 0])
     gt = np.copy(target[:, :, 1])
@@ -25,19 +25,28 @@ def PoissonBlend(source, mask, target, isMix = False):
         for i in range(0, np.size(mask, 0)):
             if mask[i][j][0] == 1.0:
                 if isMix:
-                    temp_s_R = (4*source[i][j][0]) - (source[i-1][j][0] if i > 0 else 0) - (source[i+1][j][0] if i < (np.size(mask, 0) - 1) else 0) - (source[i][j+1][0] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][0] if j > 0 else 0)
-                    temp_s_G = (4*source[i][j][1]) - (source[i-1][j][1] if i > 0 else 0) - (source[i+1][j][1] if i < (np.size(mask, 0) - 1) else 0) - (source[i][j+1][1] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][1] if j > 0 else 0)
-                    temp_s_B = (4*source[i][j][2]) - (source[i-1][j][2] if i > 0 else 0) - (source[i+1][j][2] if i < (np.size(mask, 0) - 1) else 0) - (source[i][j+1][2] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][2] if j > 0 else 0)
-                    temp_t_R = (4*target[i][j][0]) - (target[i-1][j][0] if i > 0 else 0) - (target[i+1][j][0] if i < (np.size(mask, 0) - 1) else 0) - (target[i][j+1][0] if j < (np.size(mask, 1) - 1) else 0) - (target[i][j-1][0] if j > 0 else 0)
-                    temp_t_G = (4*target[i][j][1]) - (target[i-1][j][1] if i > 0 else 0) - (target[i+1][j][1] if i < (np.size(mask, 0) - 1) else 0) - (target[i][j+1][1] if j < (np.size(mask, 1) - 1) else 0) - (target[i][j-1][1] if j > 0 else 0)
-                    temp_t_B = (4*target[i][j][2]) - (target[i-1][j][2] if i > 0 else 0) - (target[i+1][j][2] if i < (np.size(mask, 0) - 1) else 0) - (target[i][j+1][2] if j < (np.size(mask, 1) - 1) else 0) - (target[i][j-1][2] if j > 0 else 0)
+                    temp_s_R = (4*source[i][j][0]) - (source[i-1][j][0] if i > 0 else 0) - (source[i+1][j][0] if i < (np.size(
+                        mask, 0) - 1) else 0) - (source[i][j+1][0] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][0] if j > 0 else 0)
+                    temp_s_G = (4*source[i][j][1]) - (source[i-1][j][1] if i > 0 else 0) - (source[i+1][j][1] if i < (np.size(
+                        mask, 0) - 1) else 0) - (source[i][j+1][1] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][1] if j > 0 else 0)
+                    temp_s_B = (4*source[i][j][2]) - (source[i-1][j][2] if i > 0 else 0) - (source[i+1][j][2] if i < (np.size(
+                        mask, 0) - 1) else 0) - (source[i][j+1][2] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][2] if j > 0 else 0)
+                    temp_t_R = (4*target[i][j][0]) - (target[i-1][j][0] if i > 0 else 0) - (target[i+1][j][0] if i < (np.size(
+                        mask, 0) - 1) else 0) - (target[i][j+1][0] if j < (np.size(mask, 1) - 1) else 0) - (target[i][j-1][0] if j > 0 else 0)
+                    temp_t_G = (4*target[i][j][1]) - (target[i-1][j][1] if i > 0 else 0) - (target[i+1][j][1] if i < (np.size(
+                        mask, 0) - 1) else 0) - (target[i][j+1][1] if j < (np.size(mask, 1) - 1) else 0) - (target[i][j-1][1] if j > 0 else 0)
+                    temp_t_B = (4*target[i][j][2]) - (target[i-1][j][2] if i > 0 else 0) - (target[i+1][j][2] if i < (np.size(
+                        mask, 0) - 1) else 0) - (target[i][j+1][2] if j < (np.size(mask, 1) - 1) else 0) - (target[i][j-1][2] if j > 0 else 0)
                     rt[i][j] = (temp_s_R if temp_s_R >= temp_t_R else temp_t_R)
                     gt[i][j] = (temp_s_G if temp_s_G >= temp_t_G else temp_t_G)
                     bt[i][j] = (temp_s_B if temp_s_B >= temp_t_B else temp_t_B)
                 else:
-                    rt[i][j] = (4*source[i][j][0]) - (source[i-1][j][0] if i > 0 else 0) - (source[i+1][j][0] if i < (np.size(mask, 0) - 1) else 0) - (source[i][j+1][0] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][0] if j > 0 else 0)
-                    gt[i][j] = (4*source[i][j][1]) - (source[i-1][j][1] if i > 0 else 0) - (source[i+1][j][1] if i < (np.size(mask, 0) - 1) else 0) - (source[i][j+1][1] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][1] if j > 0 else 0)
-                    bt[i][j] = (4*source[i][j][2]) - (source[i-1][j][2] if i > 0 else 0) - (source[i+1][j][2] if i < (np.size(mask, 0) - 1) else 0) - (source[i][j+1][2] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][2] if j > 0 else 0)
+                    rt[i][j] = (4*source[i][j][0]) - (source[i-1][j][0] if i > 0 else 0) - (source[i+1][j][0] if i < (np.size(mask, 0) - 1)
+                                                                                            else 0) - (source[i][j+1][0] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][0] if j > 0 else 0)
+                    gt[i][j] = (4*source[i][j][1]) - (source[i-1][j][1] if i > 0 else 0) - (source[i+1][j][1] if i < (np.size(mask, 0) - 1)
+                                                                                            else 0) - (source[i][j+1][1] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][1] if j > 0 else 0)
+                    bt[i][j] = (4*source[i][j][2]) - (source[i-1][j][2] if i > 0 else 0) - (source[i+1][j][2] if i < (np.size(mask, 0) - 1)
+                                                                                            else 0) - (source[i][j+1][2] if j < (np.size(mask, 1) - 1) else 0) - (source[i][j-1][2] if j > 0 else 0)
 
                 row.append((j*np.size(mask, 0) + i))
                 col.append((j*np.size(mask, 0) + i))
@@ -66,7 +75,8 @@ def PoissonBlend(source, mask, target, isMix = False):
                 col.append((j*np.size(mask, 0) + i))
                 data.append(1)
 
-    A = scipy.sparse.csr_matrix((data, (row, col)), shape=(np.size(mask, 0)*np.size(mask, 1), np.size(mask, 0)*np.size(mask, 1)))
+    A = scipy.sparse.csr_matrix((data, (row, col)), shape=(
+        np.size(mask, 0)*np.size(mask, 1), np.size(mask, 0)*np.size(mask, 1)))
 
     rt = np.reshape(rt, ((np.size(rt, 0)*np.size(rt, 1)), 1), order="F")
     gt = np.reshape(gt, ((np.size(gt, 0)*np.size(gt, 1)), 1), order="F")
@@ -76,9 +86,12 @@ def PoissonBlend(source, mask, target, isMix = False):
     result_g = scipy.sparse.linalg.spsolve(A, gt)
     result_b = scipy.sparse.linalg.spsolve(A, bt)
 
-    result_r = np.reshape(result_r, (np.size(target, 0), np.size(target, 1)), order="F")
-    result_g = np.reshape(result_g, (np.size(target, 0), np.size(target, 1)), order="F")
-    result_b = np.reshape(result_b, (np.size(target, 0), np.size(target, 1)), order="F")
+    result_r = np.reshape(result_r, (np.size(target, 0),
+                          np.size(target, 1)), order="F")
+    result_g = np.reshape(result_g, (np.size(target, 0),
+                          np.size(target, 1)), order="F")
+    result_b = np.reshape(result_b, (np.size(target, 0),
+                          np.size(target, 1)), order="F")
 
     result_r[result_r < 0.0] = 0.0
     result_r[result_r > 1.0] = 1.0
@@ -88,8 +101,9 @@ def PoissonBlend(source, mask, target, isMix = False):
     result_b[result_b > 1.0] = 1.0
 
     result = np.stack((result_r, result_g, result_b), axis=2)
-    
+
     return result
+
 
 def Read(path="", source_filename=""):
     source = plt.imread(path + source_filename)
@@ -116,8 +130,6 @@ def extract_patch(image, x, y, patch_height, patch_width):
     y = int(y)
     patch_height = int(patch_height)
     patch_width = int(patch_width)
-    # print(image.shape)
-    # print(x, y, patch_height, patch_width)
     return image[y:y+patch_height, x:x+patch_width]
 
 
@@ -128,8 +140,9 @@ def find_best_match(image, border_patch, patch_size, mask):
     for y in range(image.shape[0] - patch_size[0] + 1):
         for x in range(image.shape[1] - patch_size[1] + 1):
             patch = extract_patch(image, x, y, patch_size[0], patch_size[1])
-            patch_mask = extract_patch(mask, x, y, patch_size[0], patch_size[1])
-            
+            patch_mask = extract_patch(
+                mask, x, y, patch_size[0], patch_size[1])
+
             if np.any(patch_mask == 1):
                 continue
 
@@ -173,11 +186,7 @@ if __name__ == '__main__':
     mask_area_height, mask_area_width = y2 - y1, x2 - x1
     source_patch = extract_patch(
         source, best_x + border_size, best_y + border_size, mask_area_height, mask_area_width)
-    # source[y1:y2, x1:x2] = source_patch
-    print(int(y1), int(y2), int(x1), int(x2))
-    print(source_patch.shape)
-    # plt.imshow(source_patch)
-    # plt.show()
+
     plt.imsave('source_patch.png', source_patch)
     plt.imsave('initial_patch.png', initial_patch)
 
